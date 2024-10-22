2cbf403319a5203c994a0b331b454fd93d4b3899
Md Repon <repon7253@gmail.com>
2024-10-22 16:28:20 +0600

    fix error

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-cors({
+app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
-});
+}));
 
 app.use(express.json());
 
