43318b35879e6562f64f2669bac5da557d0a964a
Md Repon <repon7253@gmail.com>
2024-11-13 12:17:31 +0600

    F1  course sorting logic

--- controllers/student-controller/course-countroller.js
+++ controllers/student-controller/course-countroller.js
@@ -2,15 +2,47 @@ const Course = require("../../models/Course");
 
 const getAllStudentViewCours = async (req, res) => {
   try {
-    const coursesList = await Course.find({});
+    const {
+      category = [],
+      level = [],
+      primaryLanguage = [],
+      sortBy = "price-lowtohigh",
+    } = req.query;
 
-    if (coursesList.length === 0) {
-      return res.status(404).json({
-        success: false,
-        message: "No courses found",
-        data: [],
-      });
+    let filters = {};
+
+    if (category.length) {
+      filters.category = { $in: category.split(",") };
     }
+    if (level.length) {
+      filters.level = { $in: level.split(",") };
+    }
+    if (primaryLanguage.length) {
+      filters.primaryLanguage = { $in: primaryLanguage.split(",") };
+    }
+
+    let sortParams = {};
+
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sortParams.pricing = 1;
+        break;
+      case "price-hightolow":
+        sortParams.pricing = -1;
+        break;
+      case "title-atoz":
+        sortParams.title = 1;
+        break;
+      case "title-ztoa":
+        sortParams.title = -1;
+        break;
+
+      default:
+        sortParams.pricing = 1;
+        break;
+    }
+
+    const coursesList = await Course.find(filters).sort(sortParams);
 
     res.status(200).json({
       success: true,
